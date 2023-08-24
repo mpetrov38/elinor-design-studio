d4d9886b6e74fc1c991326e74a7c53fe1887291b
Milen <petrovmilen38@gmail.com>
2023-08-24 12:05:22 +0300

    styled footer,html finished

--- elinor-design-studio/src/components/Footer/Footer.js
+++ elinor-design-studio/src/components/Footer/Footer.js
@@ -1,8 +1,59 @@
 import React from 'react'
+import logo from '../../images/logo_dark_text_4.png';
+import { FaFacebookF } from 'react-icons/fa';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { BiLogoLinkedin } from 'react-icons/bi';
+import { TiMail } from 'react-icons/ti';
+import { HiOutlinePhone } from 'react-icons/hi';
+import './Footer.css';
 
 function Footer() {
   return (
-    <span>Footer</span>
+    <div className='footer'>
+      <div className='upper-footer-section'>
+        <span>
+          <img src={logo} />
+          <span>
+            <FaFacebookF />
+            <AiOutlineInstagram />
+            <BiLogoLinkedin />
+          </span>
+        </span>
+        <span>
+          <h3>PAGES</h3>
+          <span>Services</span>
+          <span>Portfolio</span>
+          <span>About</span>
+          <span>Shop</span>
+          <span>Contact</span>
+        </span>
+        <span>
+          <h3>USEFUL LINKS</h3>
+
+          <span>Useful links</span>
+          <span>Privacy Policy</span>
+          <span>Cookie Policy</span>
+          <span>Press</span>
+          <span>Journal</span>
+        </span>
+        <span>
+          <h3>CONTACTS</h3>
+          <span><TiMail />studio@elinorinteriors.com</span>
+          <span><HiOutlinePhone />081122345 / Sofia, Bulgaria</span>
+          <span><HiOutlinePhone />081122345 / New York, USA</span>
+        </span>
+        <span>
+          <h3>NEWSLETTER</h3>
+          <span>Email address</span>
+          <input type='text' placeholder='ivan.ivanov@gmail.com'></input>
+          <span>Subscribe</span>
+        </span>
+      </div>
+      <div className='lower-footer-section'>
+        <span>Copyright 2023 Elinor Design Studio</span>
+        <span>Made with love and code by Fundamental studio</span>
+      </div>
+    </div>
   )
 }
 
--- elinor-design-studio/src/components/Home/Home.js
+++ elinor-design-studio/src/components/Home/Home.js
@@ -3,8 +3,11 @@ import signature from '../../images/signature.png';
 import heroImg from '../../images/hero image.png';
 import projectTintyava from '../../images/Rectangle 5.png';
 import projectMandur from '../../images/About us image.png';
-import aboutUsImg from '../../images/About us image (1).png' 
-
+import aboutUsImg from '../../images/About us image (1).png';
+import shopImage from '../../images/Rectangle 13.png';
+import shopImageTwo from '../../images/Rectangle 14.png';
+import shopImageThree from '../../images/Rectangle 15.png';
+import blogPic from '../../images/blue pic 1.png';
 import './Home.css';
 import { Link } from 'react-router-dom';
 
@@ -55,15 +58,78 @@ function Home() {
         <div>
           <div className='about-us-card'>
             <img src={aboutUsImg}></img>
-
             <div className='about-us-text'>
-
               <h2>Redifining luxury</h2>
               <p>Luxury in interior design has evolved, and redifining its meaning is <br /> at the heart of our philosophy and design approach.</p>
               <span><Link to="/services">VIEW SERVICES</Link></span>
-
             </div>
+          </div>
+        </div>
+      </div>
+      <div className='shop-favorites-text'>
+        <h2>Shop our favorites</h2>
+        <span><Link to="/shop">Shop All</Link></span>
+      </div>
+      <div className='shop-favorites-container'>
+        <div className='shop-card'>
+          <img src={shopImage}></img>
+          <div className='card-text'>
+            <span className='card-title'>Vintage chair</span>
+            <span><Link to="/shop">SHOP</Link></span>
+          </div>
+        </div>
+        <div className='shop-card'>
+          <img src={shopImageTwo}></img>
+          <div className='card-text'>
+            <span className='card-title'>Vintage chair</span>
+            <span><Link to="/shop">SHOP</Link></span>
+          </div>
+        </div>
+        <div className='shop-card'>
+          <img src={shopImageThree}></img>
+          <div className='card-text'>
+            <span className='card-title'>Vintage chair</span>
+            <span><Link to="/shop">SHOP</Link></span>
+          </div>
+        </div>
+      </div>
 
+      <div className='blog-container'>
+        <div className='shop-favorites-text'>
+          <h2>Check out our blog</h2>
+          <span><Link to="/blog">VIEW ALL</Link></span>
+        </div>
+
+        <div className='latest-projects-cards latest-projects-container'>
+          <div className='card'>
+            <img src={blogPic}></img>
+            <div className='card-text blog-text'>
+              <span>Dec 23,2015<li>5 min read</li></span>
+              <h2>Blog article heading here</h2>
+              <span>As a warehouse manager, you would understand that handling warehouse <br /> operations are a massive task </span>
+              <span><Link to="/portfolio">READ MORE</Link></span>
+            </div>
+          </div>
+          <div className='card'>
+            <img src={blogPic}></img>
+            <div className='card-text blog-text'>
+              <span>Dec 23,2015<li>5 min read</li></span>
+              <h2>Blog article heading here</h2>
+              <span>As a warehouse manager, you would understand that handling warehouse <br /> operations are a massive task </span>
+              <span><Link to="/portfolio">READ MORE</Link></span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='about-us-services-container'>
+        <div className='about-us-card'>
+          <div className='about-us-text'>
+            <h2>Redifining minimalistic design <br /> for your own home</h2>
+            <p>To create timeless, interior spaces that inspire tranquility,
+              <br /> elevate functionality, and reflect the unique personality and
+              <br />value of our client.
+            </p>
+            <span><Link to="/contact">SEND AN ENQUIRE</Link></span>
           </div>
         </div>
       </div>
